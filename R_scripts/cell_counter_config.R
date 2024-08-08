2ec0659561474486a969a3e7de5a64f65e2e3eeb
Phillip Muza <phillipmuza@gmail.com>
2024-08-08 09:18:04 +0100

    Added new code and documentation in R scripts

--- R_scripts/cell_counter_config.R
+++ R_scripts/cell_counter_config.R
@@ -1,9 +1,10 @@
 #### GENERIC SCRIPT TO CALCULATE VOLUMES AND ANNOTATE CELLS IN APPROPRIATE BRAIN REGIONS 
 
-  #### PHILLIP MUZA - 11.08.2022 ####
+#### PHILLIP MUZA - 11.08.2022 ####
 
-  ## UPDATED: to include sizes of cells - 04.12.2023
-  
+## UPDATED: to include sizes of cells - 04.12.2023
+## UPDATED: saves separate files for cells numbers and volumes per region, and descriptors of cell volumes
+
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(tidyverse,
                stringr,
@@ -41,7 +42,7 @@ upload_cells_xyz <- function(csv_file) {
   df = subset(df, select = -c(Label, X)) 
   colnames(df) <- c("Object", "Volume", "x", "y", "z")
   #Bonej particle analyser outputs the real spatial location of centriods
-    #so you will need to divide by the pixel resolution to map the centriods to the same space as the autofluorescence & annotation images
+  #so you will need to divide by the pixel resolution to map the centriods to the same space as the autofluorescence & annotation images
   df$x <- as.numeric(as.character(df$x))/ pixel_resolution 
   df$y <- as.numeric(as.character(df$y))/ pixel_resolution
   df$z <- as.numeric(as.character(df$z))/ pixel_resolution
@@ -66,21 +67,37 @@ volume_calculation <- function(auto_slice){
 
 ### 2. Functions to collate Cell Numbers by Anatomical Location -----------------------------
 
-#This function combines the coordinates of the cells with the coordinates of the
-  #anatomical region - it will also save a csv of this data
-cell_locations <- function(cell_coordinates, region_coordinates){
-  df <- inner_join(cell_coordinates, region_coordinates) #Combine cells with region coordinates
-  df_sum <- df %>% 
-    count(id, cell_size)
+# Function to count the number of cells in each region, with an optional argument to include cell size
+cell_locations <- function(cell_coordinates, region_coordinates, include_cell_size = FALSE){
+  
+  # Combine cells with region coordinates
+  df <- inner_join(cell_coordinates, region_coordinates)
+  
+  # Count the number of cells in each region
+  if (include_cell_size) {
+    # Count the number of cells in each region, including cell size
+    df_sum <- df %>% 
+      count(id, cell_size)
+  } else {
+    # Count the number of cells in each region, without cell size
+    df_sum <- df %>% 
+      count(id)
+  }
+  
+  # Convert id column to factor
   df_sum$id <- as.factor(df_sum$id)
-  decoded_df <- inner_join(df_sum, atlas_tree) #This gives a df of the regions present in the dataset and number of cells/regions
+  
+  # Join with atlas tree to get region information
+  decoded_df <- inner_join(df_sum, atlas_tree) 
+  
+  # Return the resulting data frame
   return(decoded_df)
 }
 
 ### 3. Add column giving a qualitative description of cell size -------------------------------
 
 #Subset unique volumes in cell_xyz dataframe into threes
-  #his only works when you have a list of =< 3 unique volumes 
+#his only works when you have a list of =< 3 unique volumes 
 subset_volumes_in_threes <- function(dataframe) {
   volumes <- c(unique(dataframe$Volume)) #Create a list of the unique values of volumes
   volumes_sorted <- sort(volumes) #Sort volumes in ascending order
@@ -89,71 +106,109 @@ subset_volumes_in_threes <- function(dataframe) {
   volume_chunks <- setNames(as.list(volume_chunks), sizes) #Give volume chunks names
   volume_chunks_df <- as.data.frame(do.call(cbind, volume_chunks)) #convert list of volume_chunks into a dataframe
   return(volume_chunks_df)
-  }
+}
 
 #This function identifies a unique volume value and categories it based on its size
 get_column_name <- function(dataframe, target_value) {
   for (col in names(dataframe)) {
-    if (target_value %in% dataframe[[col]]) { #if the unique volume value is in the volumes_df it will return description of volume size (column name)
+    
+    #if the unique volume value is in the volumes_df it will return description of volume size (column name)
+    if (target_value %in% dataframe[[col]]) { 
       return(col)
     }
   }
-  return(print(paste(target_value, ' - Volume value not identified'))) #otherwise it will return an error
+  #otherwise it will return an error
+  return(print(paste(target_value, ' - Volume value not identified'))) 
 }
 
-#This function will categorise cell volumes in cell_xyz
+# This function will categorise cell volumes in cell_xyz
 categorise_cell_volumes <- function(){ 
-  cells_xyz$cell_size <- NA #Create an empty column to add descriptive cell sizes to
-  list_of_volumes <- c(cells_xyz$Volume) #generate list of all volumes in cells_xyz
-  #if your list of volumes is less than 3 it will return a WARNING
+  #Create an empty column to add descriptive cell sizes to
+  cells_xyz$cell_size <- NA 
+  
+  #generate list of all volumes in cells_xyz
+  list_of_volumes <- c(cells_xyz$Volume) 
+  
+  #if your list of volumes is less than 3 it will return a WARNING otherwise it will create a new column categorising your volumes
   if (length(unique(list_of_volumes)) < 3){
     print(paste("WARNING: Only ", length(unique(list_of_volumes)), " unique volume values in ", getwd()))
     return(cells_xyz)
-  #if your list of volumes is 3 or more it will create a new column categorising your volumes 
   }  else{ 
-    names_of_cells <- list() #initialise an empty list which will contain the cell volume categories
-    cell_size_descriptions <- subset_volumes_in_threes(cells_xyz) #create dataframe with unique cell volume descriptions
-  #This for loop will add all the cell categories into a list 
+    #initialise an empty list which will contain the cell volume categories
+    names_of_cells <- list() 
+    #create dataframe with unique cell volume descriptions
+    cell_size_descriptions <- subset_volumes_in_threes(cells_xyz) 
+    
+    #This for loop will add all the cell categories into a list 
     for (i in list_of_volumes){
       names_of_cells <- append(names_of_cells, get_column_name(cell_size_descriptions, i)) 
-  }
-    cells_xyz[["cell_size"]] <- names_of_cells #Add descriptive cells sizes to 'cell_size' column in cells_xyz
+    }
+    
+    #Add descriptive cells sizes to 'cell_size' column in cells_xyz
+    cells_xyz[["cell_size"]] <- names_of_cells
     return(cells_xyz) }
 }
 
 ### 4. Functions to combine Cell Numbers and Region Volumes  --------------------------------
 
 cell_and_volume_df <- function(cell_regions, volume_regions, save_filename){
+  # Perform inner join on cell regions and volume regions
   df <- inner_join(cell_regions, volume_regions)
-  df <- subset(df, select = -c(Freq))
-  df <- df[, c("name","acronym","structure_id_path","id",
-               "parent_structure_id","n", "cell_size", "volume(mm^3)")]
-  colnames(df)[8] <- 'region_volume(mm^3)'
-  df$cell_size <- sapply(df$cell_size, paste, collapse = '')
-  write.csv(df, file = save_filename)
+  
+  # Remove Freq column
+  df <- df %>% 
+    select(-Freq)
+  
+  # Check if cell_size column is present
+  if ("cell_size" %in% colnames(df)) {
+    # Subset columns including cell_size, remove commas from cell_size, and rename volume column
+    df <- df %>% 
+      select(name, acronym, structure_id_path, id, parent_structure_id, n, cell_size, volume_mm3 = `volume(mm^3)`) %>% 
+      mutate(cell_size = gsub(",", "", cell_size))
+  } else {
+    # Subset columns without cell_size and rename volume column
+    df <- df %>% 
+      select(name, acronym, structure_id_path, id, parent_structure_id, n, volume_mm3 = `volume(mm^3)`)
+  }
+  
+  # Write data frame to CSV file
+  write.csv(df, file = save_filename, row.names = FALSE)
+  
+  # Return data frame
   return(df)
 }
 
-            
+
 ### Importing your files  -----
 
 auto_xyz <- upload_autofluorescence_xyz(autofluorescence_file) #autofluorescence file 
 anno_xyz <- upload_annotation_xyz(annotation_file) #annotations file
 
 #THIS IS IMPORTANT - THIS NEXT LINE MERGES PIXELS PRESENT IN AUTOFLUO SLICE WITH ANNOTATIONS 
-  #WHAT YOU WILL HAVE IS A FILE WITH ANATOMICAL IDS OF THE PIXEL LOCATIONS IN YOUR RAW DATA
+#WHAT YOU WILL HAVE IS A FILE WITH ANATOMICAL IDS OF THE PIXEL LOCATIONS IN YOUR RAW DATA
 pixel_ids_xyz <- inner_join(auto_xyz, anno_xyz)
 
-cells_xyz <- upload_cells_xyz(cell_centroids_file) #segmented cells and locations
+# Segmented cells and locations
+cells_xyz <- upload_cells_xyz(cell_centroids_file) 
 
-cells_xyz <- categorise_cell_volumes() #add column qualitatively describing cell sizes
+# Add column qualitatively describing cell sizes
+cells_with_volume_descriptors <- categorise_cell_volumes() 
 
+# .csv with decoded brain regions 
 atlas_tree <- read.csv(decoded_atlas_file)
 atlas_tree$id <- as.factor(atlas_tree$id)
 
 # Merging Data from Autofluorescence and Transformed Atlas 
-region_volumes <- volume_calculation(pixel_ids_xyz)
-cell_regions <- cell_locations(cells_xyz, pixel_ids_xyz)
-combined_cells_volumes <- cell_and_volume_df(cell_regions, region_volumes, "cell_and_region_volumes.csv")
+region_volumes <- volume_calculation(pixel_ids_xyz) # Volumes of the regions in your brain slice
+
+# Count your cells per brain region
+  # Without cell sizes
+cell_regions <- cell_locations(cells_xyz, pixel_ids_xyz) # count your cells per region
+  
+  # With cell sizes
+cell_regions_with_volume_descriptors <- cell_locations(cells_with_volume_descriptors, pixel_ids_xyz, include_cell_size = TRUE) # count your cells per region including cell volume descriptors 
+
+cell_and_volume_df(cell_regions, region_volumes, "cell_and_region_volumes.csv") # combine cell and volume data
+cell_and_volume_df(cell_regions_with_volume_descriptors, region_volumes, "cell_and_region_volumes_with_volume_descriptors.csv") # combine cell and volume data
 
 
--- R_scripts/cell_counting_pipeline.R
+++ R_scripts/cell_counting_pipeline.R
@@ -3,28 +3,39 @@
     ## 12.08.22 ##
 
 #Set your parameters here:
-pixel_resolution = 10 #This should be isometric so you only need one value in um
-autofluorescence_file = "auto_xyz.txt" #The file with the pixel coordinates of autofluorescent image
-annotation_file = "annotation_xyz.txt" #The file with the pixel coordinates of annotation image
-cell_centroids_file = "objects_xyz.csv" #The file with the coordinates of your cells
-decoded_atlas_file = "path/to/atlas_file" #The PATH to your atlas file 
-cell_counter = "path/to/cell_counter_config.R" # The PATH to the cell counter script 
+  # This should be isometric so you only need one value in um
+pixel_resolution = 10 
 
-#Enter the path to your directory with the folders with your slice data
+  # The file with the pixel coordinates of autofluorescent image
+autofluorescence_file = "auto_downsampled_xyz.txt" 
+
+  # The file with the pixel coordinates of annotation image
+annotation_file = "annotation_xyz.txt" 
+
+  # The file with the coordinates of your cells
+cell_centroids_file = "objects_xyz.csv" 
+
+  # The PATH to your atlas file
+decoded_atlas_file = "path/to/atlas_file"  
+
+  # The PATH to the cell counter script
+cell_counter = "path/to/cell_counter_config.R"  
+
+
+# Enter the path to your directory with the folders with your slice data
 parent.folder <- "path/to/folder_with_slice_data"
 
-#This line assigns all the folders within your parent directory to a vector
+# This line assigns all the folders within your parent directory to a vector
 sub.folders <- list.dirs(parent.folder, recursive = FALSE, full.names = TRUE)
 
-#This line will allow you to filter out only the relevant folder you want to analyse
-  #It is good practice to keep a common name to identify your folders of interest
-    #Here I have used "AN" - change the green text to your identifier
-#sub.folders <- sub.folders[grepl("AN53", sub.folders)]
+# This line will allow you to filter out only the relevant folder you want to analyse
+  # It is good practice to keep a common name to identify your folders of interest
+    # Here I have used "AN" - change the green text to your identifier
+# sub.folders <- sub.folders[grepl("AN53", sub.folders)]
 
 r.script <- file.path(cell_counter)
 
-#This for loop will iterate through your folders of interest and run the script
-  #the script is the cell_counter_config.R file
+# This for loop will iterate through your folders of interest and run  cell_counter_config.R file
 for (i in sub.folders) {
   setwd(i)
   working_directory <- getwd()
